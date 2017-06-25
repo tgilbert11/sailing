14a2ca38010915c4ee283018f261ddde77907b12
Taylor Gilbert <taylorg353@gmail.com>
2017-06-24 20:47:56 -0700

    Initial Commit

---
+++ sailingTests/sailingTests.swift
@@ -0,0 +1,36 @@
+//
+//  sailingTests.swift
+//  sailingTests
+//
+//  Created by Taylor H. Gilbert on 6/24/17.
+//  Copyright © 2017 Taylor H. Gilbert. All rights reserved.
+//
+
+import XCTest
+@testable import sailing
+
+class sailingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
